4898f8adbdc74c66ae8b6d6b3d07fc3e0902a17e
Daniel Burruchaga <dbs99@hotmail.com>
2022-02-24 17:06:44 +0100

    Delete createCron.sh

--- createCron.sh
+++
@@ -1,6 +0,0 @@
-
-#echo new cron into cron file
-echo '@reboot /home/pi/gateway-Service/init-python.sh > /tmp/HUMTEMP_LEDS_SWITCH.log 2>&1' >> mycron_service
-crontab mycron_service
-echo '@reboot sudo /home/pi/gatewayServiceConfig/init-python.sh > /tmp/port_7777.log 2>&1' >> mycron_port
-crontab mycron_port
